2a1e08ad31d9068f6bc5a78ddd81c250f8c46be4
yashmimrot <yashmimrot@gmail.com>
2025-01-18 20:46:48 +0530

    initial commit

--- src/main/java/videverse/vv_editor/VvEditorApplication.java
+++ src/main/java/videverse/vv_editor/VvEditorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VvEditorApplication {
 
 	public static void main(String[] args) {
+		//starts here
 		SpringApplication.run(VvEditorApplication.class, args);
 	}
 
